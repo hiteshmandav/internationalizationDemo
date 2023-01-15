09e1a62c34882db223eb788b9b09a2aa152a1034
Hitesh Mandav <mandavhitesh@gmail.com>
2023-01-15 19:21:04 +0530

    add language picker dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'internationalizationDemo';
+
+  public selectedLanguage: string;
+
+  public supportedLanguages = [
+    {
+      id: "en-US",
+      label: "English-USA",
+    },{
+      id: "en-IN",
+      label: "English-INDIA",
+    },{
+      id: "hi-IN",
+      label: "Hindi-INDIA",
+    },{
+      id: "fr-FR",
+      label: "French-France",
+    },{
+      id: "es-ES",
+      label: "Espaniol-Spain",
+    },
+  ]
+
+  constructor(){
+    this.selectedLanguage = localStorage.getItem('selectedLanguage') || 'en-US';
+  }
+
+  public changeSelectedLanguage(selectedLanguage: any): void {
+    this.selectedLanguage = selectedLanguage;
+    localStorage.setItem('selectedLanguage', this.selectedLanguage);
+    window.location.reload();
+  }
+
+
 }
