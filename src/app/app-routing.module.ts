74d1e49ff72c7b6021b26fb4b1414c8b026557f7
Hitesh Mandav <mandavhitesh@gmail.com>
2023-01-15 18:45:29 +0530

    add order details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OrderDetailsComponent,
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
